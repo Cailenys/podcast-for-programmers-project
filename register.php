3b2c256c9bb4d4845642dfd7cc6dda7a38be0fa7
Cailenys <cailenys.salazar@gmail.com>
2020-05-24 13:11:32 -0600

    Register and Login form added

---
+++ register.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Podcast for Programmers</title>
+</head>
+<body>
+    <div id="inputContainer">
+        <form id="loginForm" action="resgister.php" method ="POST">
+            <h2> Login to your account </h2>
+            <p>
+            <label for="loginUsername"> Username </label>
+            <input id="loginUsername" name="loginUsername" type="text" placeholder ="e.g Cail" required>
+            </p>
+            <p>
+            <label for="loginPassword"> Password </label>
+            <input id="loginPassword" name="loginPassword" type="password" placeholder= "Your password" required>
+            </p>
+            <button type="submit" name="LoginButton">Login</button>
+        </form>
+
+        <form id="registerForm" action="resgister.php" method ="POST">
+            <h2> Create your free account </h2>
+
+            <p>
+            <label for="username"> Username </label>
+            <input id="username" name="username" type="text" placeholder ="e.g Cail" required>
+            </p>
+
+            <p>
+            <label for="email"> Email </label>
+            <input id="email" name="email" type="eamil" placeholder ="e.g cailenys@ualberta.ca" required>
+            </p>
+
+            <p>
+            <label for="email2"> Confim email </label>
+            <input id="email2" name="email" type="eamil2" placeholder ="e.g cailenys@ualberta.ca" required>
+            </p>
+           
+            <p>
+            <label for="password"> Password </label>
+            <input id="password" name="password" type="password" placeholder= "Your password" required>
+            </p>
+
+            <p>
+            <label for="password2"> Confirm password </label>
+            <input id="password2" name="password2" type="password"  placeholder= "Your password" required>
+            </p>
+            <button type="submit" name="registerButton">Sing up</button>
+
+        </form>
+  
+    </div>
+</body>
+</html>
