b00d4414675beedcf50a1f84052165e37eabe5b9
Cailenys <cailenys.salazar@gmail.com>
2020-06-10 09:00:04 -0600

    Your playlist seccion added to navbar

--- includes/navBarContainer.php
+++ includes/navBarContainer.php
@@ -23,7 +23,7 @@
 			</div>
 
 			<div class="navItem">
-				<span role="link" tabindex="0" onclick="openPage('yourPodcasts.php')" class="navItemLink">play</span>
+				<span role="link" tabindex="0" onclick="openPage('yourPodcasts.php')" class="navItemLink">Your playlists</span>
 			</div>
 
 			<div class="navItem">
